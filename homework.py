cbe5c2d033e443247766b0b45350e7b8d5ad3e6e
Денис Марьянский <d.maryanskiy@gmail.com>
2020-03-14 23:27:27 +0300

    updated commit

--- homework.py
+++ homework.py
@@ -8,7 +8,7 @@ class Record:
         self.amount = amount
         self.comment = comment
         self.date = date
-        if self.date != dt.date.today(): #если дата не сегодняшняя, значит она прописана явно в строковом виде
+        if type(self.date) == str: #если дата не принадлежит типу date, а вводится явно типом str, то выпполняется операции ниже
             self.date_format = "%d.%m.%Y"
             self.date = dt.datetime.strptime(self.date, self.date_format) #дата приводится к типу date
             self.date = self.date.date()
@@ -18,28 +18,28 @@ class Record:
         return self.storage[key]
 
 
-class Calculator(Record):
+class Calculator():
     def __init__(self, limit):
         self.limit = limit
         self.records = []
-        self.period = dt.date.today() - dt.timedelta(6)
 
     def add_record(self, record): #добавление новых записей
         self.records.append(record)
 
     def get_today_stats(self): #подсчет расходов или полученных калорий за сегодня
-        self.cash_delta = 0
+        cash_delta = 0
         for record in self.records:
             if record[1] == dt.date.today():
-                self.cash_delta += record[0]
-        return self.cash_delta
+                cash_delta += record[0]
+        return cash_delta
 
     def get_week_stats(self): #подсчет расходов или полученных калорий за последние 7 дней
-        self.week_delta = 0
+        week_delta = 0
+        period = dt.date.today() - dt.timedelta(6)
         for record in self.records:
-            if (record[1] >= self.period) and (record[1] <= dt.date.today()):
-                self.week_delta += record[0]
-        return self.week_delta
+            if (record[1] >= period) and (record[1] <= dt.date.today()):
+                week_delta += record[0]
+        return week_delta
 
 
 class CashCalculator(Calculator):
@@ -49,21 +49,24 @@ class CashCalculator(Calculator):
     def __init__(self, limit):
         super().__init__(limit)
     
+    def limiter(self, course = 1, currency = 'руб'): #та же самая функция, но без учета валюты, дабы избежать повторов
+        self.course = course
+        self.currency = currency
+        if self.limit > self.get_today_stats():
+            return f'На сегодня осталось {round(((self.limit - self.get_today_stats())/self.course), 2)} {self.currency}'
+        elif self.limit == self.get_today_stats():
+            return f'Денег нет, держись'
+        else:
+            return f'Денег нет, держись: твой долг - {round(((self.get_today_stats() - self.limit)/self.course), 2)} {self.currency}'
+    
     def get_today_cash_remained(self, currency): #функция подсчета оставшегося количества денег в нужной валюте
         self.currency = currency
-        def limiter(limit, cash_delta, course = 1, currency = 'руб'): #та же самая функция, но без учета валюты, дабы избежать повторов
-            if limit > cash_delta:
-                return f'На сегодня осталось {round(((limit - cash_delta)/course), 2)} {currency}'
-            elif limit == cash_delta:
-                return f'Денег нет, держись'
-            else:
-                return f'Денег нет, держись: твой долг - {round(((cash_delta - limit)/course), 2)} {currency}'
         if currency == "rub":
-            return limiter(self.limit, self.get_today_stats())
+            return self.limiter()
         elif currency == 'usd':
-            return limiter(self.limit, self.get_today_stats(), self.USD_RATE, currency = 'USD')
+            return self.limiter(self.USD_RATE, currency = 'USD')
         elif currency == 'eur':
-            return limiter(self.limit, self.get_today_stats(), self.EURO_RATE, currency = 'Euro')
+            return self.limiter(self.EURO_RATE, currency = 'Euro')
         else:
             return "Неправильная валюта"
 
